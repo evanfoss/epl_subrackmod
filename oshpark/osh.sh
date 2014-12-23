52ea20a124be1665af1e2d33f8669a3b5ab3aa21
Evan Foss <evanfoss@gmail.com>
2014-12-23 16:38:40 -0500

    initial commit prototype status

---
+++ oshpark/osh.sh
@@ -0,0 +1,11 @@
+rm edgecard-art.fab.gbr
+mv edgecard-art.bottom.gbr edgecard-art.gbl
+mv edgecard-art.bottommask.gbr edgecard-art.gbs
+mv edgecard-art.bottomsilk.gbr edgecard-art.gbo
+mv edgecard-art.outline.gbr edgecard-art.gko
+mv edgecard-art.plated-drill.cnc edgecard-art.xln
+mv edgecard-art.top.gbr edgecard-art.gtl
+mv edgecard-art.topmask.gbr edgecard-art.gts
+mv edgecard-art.topsilk.gbr edgecard-art.gto
+
+
